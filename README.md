3566600e3cd1377d86ba61bb6a38f9b5caca4586
vatsalgada <contactvgada@gmail.com>
2023-08-26 17:43:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_hacker_news_api
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_hacker_news_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_hacker_news_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
