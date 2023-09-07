b3ef3b90251ceaf475eaa02825f8eaef54bea923
vatsalgada <contactvgada@gmail.com>
2023-09-08 02:08:54 +0530

    stories provider initialised

---
+++ lib/src/blocs/stories_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'stories_bloc.dart';
+
+abstract class StoriesProvider extends InheritedWidget {
+  final StoriesBloc bloc;
+
+  StoriesProvider({Key? key, required Widget child})
+      : bloc = StoriesBloc(),
+        super(key: key, child: child);
+
+  bool updateShouldNotify(oldWidget) => true;
+
+  static StoriesBloc of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<StoriesProvider>()
+            as StoriesProvider)
+        .bloc;
+  }
+}
